aebf2070a5cbaf4ec6fafd2892f323a913d84680
Waqas Azam <waqas@mabsapps.com>
2017-10-20 16:25:44 +0500

    Second Commit with Task Listing and Insertion Logic Applied.

--- client/main.js
+++ client/main.js
@@ -1,3 +1,26 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+Tasks = new Mongo.Collection('tasks');
+
+Template.tasks.helpers({
+    tasks: function () {
+        return Tasks.find({},{sort:{createdAt:-1}});
+    }
+});
+
+Template.tasks.events({
+
+    "submit .add-task" : function(event) {
+        event.preventDefault();
+        var name = event.target.name.value;
+        Tasks.insert({
+            name: name,
+            createdAt: new Date()
+        });
+        console.log(name);
+
+        event.target.name.value = '';
+        return false;
+    }
+});
--- server/main.js
+++ server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
+Tasks = new Mongo.Collection('tasks');
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
