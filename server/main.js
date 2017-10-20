c09563aefa1ea565119ce61e233414fe7382a52f
Waqas Azam <waqas@mabsapps.com>
2017-10-20 19:33:05 +0500

    Secure and Safe Implementation for Authentication based Task List Application

--- server/main.js
+++ server/main.js
@@ -2,6 +2,22 @@ import { Meteor } from 'meteor/meteor';
 
 Tasks = new Mongo.Collection('tasks');
 
+Meteor.methods({
+    addTask: function (name) {
+        if(!Meteor.userId()){
+            throw new Meteor.error('No Access!!!');
+        }
+        Tasks.insert({
+            name: name,
+            createdAt: new Date(),
+            userId: Meteor.userId()
+        });
+    },
+    deleteTask: function (taskId) {
+        Tasks.remove(taskId);
+    }
+});
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
